33e703de36c12a781619ed475a10b9746eb1254f
Felix Palta <fpalta90@gmail.com>
2014-10-14 19:12:54 +0400

    Fixed coding Added error on opening a directory

--- main.c
+++ main.c
@@ -5,46 +5,50 @@ static const int LINE_LENGTH = 16;
 
 int main(int argc, char** argv)
 {
-    FILE* inputfile;
+    FILE* input_file;
     int c,i;
-    char linebuf[LINE_LENGTH+1];
+    char line_buf[LINE_LENGTH+1];
     if (argc != 2)
     {
         fprintf(stderr, "Usage: %s filename\n", argv[0]);
         return 1;
     }
 
-    inputfile = fopen(argv[1],"r");
-    if (!inputfile)
+    input_file = fopen(argv[1], "ar");
+    if (!input_file)
     {
-        fprintf(stderr, "File %s not found\n",argv[1]);
+        fprintf(stderr, "File %s not found\n", argv[1]);
         return 1;
     }
-    for (;;){
+    fclose(input_file);
+    input_file = fopen(argv[1], "r");
+
+    for (;;)
+    {
         int j;
         i = 0;
 
         while ( i < LINE_LENGTH)
         {
-            if ((c = fgetc(inputfile)) == EOF) break;
+            if ((c = fgetc(input_file)) == EOF) break;
             if (!isprint(c))
             {
-                linebuf[i] = ' ';
+                line_buf[i] = ' ';
             }
             else
             {
-            linebuf[i] = c;
+            line_buf[i] = c;
             }
-            printf("%02x ",c);
+            printf("%02x ", c);
             ++i;
         }
         if (i == 0) break;
-        linebuf[i] = '\0';
-        for (j = 0; j < LINE_LENGTH - i; ++ j)
+        line_buf[i] = '\0';
+        for (j = 0; j < LINE_LENGTH - i; ++j)
         {
             printf("   ");
         }
-        printf("| %s\n",linebuf);
+        printf("| %s\n", line_buf);
         if (c == EOF) break;
     }
     return 0;
